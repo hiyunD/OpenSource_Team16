cd0e1d7154480aded895c8a1bf64577348f1a5a4
hiyunD <165583175+hiyunD@users.noreply.github.com>
2024-06-19 16:55:48 +0900

    Example_002

---
+++ test_02.py
@@ -0,0 +1,53 @@
+import cv2
+import cvzone
+import math
+from ultralytics import YOLO
+
+# cap = cv2.VideoCapture('video.mp4')
+webcamera = cv2.VideoCapture(0)
+
+model = YOLO('yolov8s-pose.pt')
+
+classnames = []
+with open('classes.txt', 'r') as f:
+    classnames = f.read().splitlines()
+
+while True:
+    # ret, frame = cap.read()
+    ret, frame = webcamera.read()
+    frame = cv2.resize(frame, (640, 480))
+
+    results = model(frame)
+
+    for info in results:
+        parameters = info.boxes
+        for box in parameters:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            confidence = box.conf[0]
+            class_detect = box.cls[0]
+            class_detect = int(class_detect)
+            class_detect = classnames[class_detect]
+            conf = math.ceil(confidence * 100)
+
+            height = y2 - y1
+            width = x2 - x1
+            threshold = height - width
+            
+            if conf > 70 and class_detect == 'person':
+                cvzone.cornerRect(frame, [x1, y1, width, height], l=30, rt=6)
+                cvzone.putTextRect(frame, f'{class_detect}', [x1 + 8, y1 - 12], thickness=2, scale=2)
+
+            if threshold < 0:
+                cvzone.putTextRect(frame, 'Fall Detected', [height, width], thickness=2, scale=2)
+
+            else:pass
+        
+    cv2.imshow('frame', frame)
+    key = cv2.waitKey(1)
+    if key == ord('q') or key == 27:  # 'q' key or 'ESC' key (27)
+        break
+
+# cap.release()
+webcamera.release()
+cv2.destroyAllWindows()
